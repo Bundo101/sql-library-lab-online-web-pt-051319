6fdd79de0dc1ba27fa85baff636b623bfd6add85
James Britton <brittonjp@gmail.com>
2019-07-31 11:47:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
-  GROUP BY series.title
+  GROUP BY series.title, characters.species
   "
 end
 
